e6742c93296d5b4bb1ab3b61c30d5eb0f2485a9d
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-24 19:01:16 +0300

    refactor: Separate the code to a separate screen for better code structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/intro_screens.dart';
 
 void main() {
   runApp(const GlobeApp());
@@ -10,34 +11,6 @@ class GlobeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Globo Fitness')),
-            body: Container(
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage("assets/gym.jpg"),
-                fit: BoxFit.cover,
-              )),
-              child: Center(
-                  child: Container(
-                padding: const EdgeInsets.all(24),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Colors.white70,
-                ),
-                child: const Text('Train Insane or Stay the Same!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 22,
-                      shadows: [
-                        Shadow(
-                          offset: Offset(1.0, 1.0),
-                          blurRadius: 2.0,
-                          color: Colors.grey
-                        )
-                      ],
-                    )),
-              )),
-            )));
+      theme: ThemeData(primarySwatch: Colors.blueGrey), home: const IntroScreen());
   }
 }
